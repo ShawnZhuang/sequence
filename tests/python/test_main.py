babb5c3b213c49a45d6486cd2cd3f30b6ed33993
Shawn Zhuang <shawn.zhuang@outlook.com>
2025-10-14 08:30:04 +0000

    add a naive demo

---
+++ tests/python/test_main.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def predict(seq_pad: np.ndarray, n_valid: int):
+    # todo: add prediction model here
+    seq_pad[n_valid]=seq_pad[n_valid-1]
+    return seq_pad
+
+def process(seq: np.ndarray, n_pred):   
+    n_prefill=seq.shape[-1]
+    x_mean=np.mean(seq,-1)
+    # pre process
+    x_normal=np.divide( np.subtract(seq, x_mean), x_mean)
+    # predict
+    x_pad=np.pad(x_normal, (0, n_pred), 'constant', constant_values=(0, 0))
+    # print(x_pad)
+    nvalid=x_normal.shape[-1]
+    for i in range(n_pred):
+        predict(x_pad, nvalid+i)   
+    # post process
+    rec=np.add(np.multiply(x_pad, x_mean), x_mean)
+    return rec
+
+data=np.array([ x for x in range (32)])
+# print(data)
+ret=process(data, 4)
+print(ret)
